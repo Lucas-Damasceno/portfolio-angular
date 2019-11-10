c58c19d796db96b33c231c78faf59df29a28bf7b
Lucas Alves <lucas_d1@outlook.com>
2019-11-10 17:23:20 -0300

    Updated some things, going to illustration

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  socialMedia = [
+    {
+      name: 'Linkedin',
+      icon:'linkedin',
+      link:'https://www.linkedin.com/in/lucasdalves/',
+      humanLink: 'linkedin.com/in/lucasdalves/'
+    },
+    {
+      name: 'GitHub',
+      icon:'github',
+      link:'https://github.com/Lucas-Damasceno',
+      humanLink: 'github.com/Lucas-Damasceno'
+    },
+    {
+      name: 'Behance',
+      icon:'behance',
+      link:'https://www.behance.net/LucasDAlves',
+      humanLink: 'behance.net/LucasDAlves'
+    }
+
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
